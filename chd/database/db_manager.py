dedf11c80cf1275efa96ae6396914d36d47f11cc
EzequielFerreira35 <ezequiel.sousa1789@gmail.com>
2026-08-07 15:15:07 -0300

    Estrutura dos arquivos adicionados, algumas func em db_manager adicionadas e mudança da func removerpacientes

---
+++ chd/database/db_manager.py
@@ -0,0 +1,87 @@
+import json
+import caminhos
+from pathlib import Path
+
+def carregar_json(caminho_arquivo: Path, nome: str) -> dict:
+    try:
+        with open(caminho_arquivo, 'r', encoding='utf-8') as arquivo:
+            dados_carregados =json.load(arquivo)
+        return dados_carregados
+    
+    except FileNotFoundError as error:
+        raise FileNotFoundError(f"Arquivo {nome}.json não existe!") from error
+        
+    except json.JSONDecodeError as error:
+        raise ValueError(f"Arquivo {nome}.json está corrompido!") from error
+
+
+
+def salvar_json(caminho_arquivo: Path, nome: str, dados: dict) -> bool:
+
+    dados_carregados = carregar_json(caminho_arquivo, nome)
+    dados_carregados[nome].append(dados)
+
+    try:
+        with open(caminho_arquivo, 'w', encoding='utf-8') as arquivo:
+            json.dump(dados_carregados, arquivo, indent=4)
+        return True
+    
+    except FileNotFoundError as error:
+        raise FileNotFoundError(f"Arquivo {nome}.json não existe!") from error
+       
+    except json.JSONDecodeError as error:
+        raise ValueError(f"Arquivo {nome}.json está corrompido!") from error
+
+
+
+def carregar_pacientes() -> dict:
+    dados = carregar_json(caminhos.JSON_PACIENTES, "pacientes")
+    return dados
+
+def carregar_medicos() -> dict:
+    dados = carregar_json(caminhos.JSON_MEDICOS, "medicos")
+    return dados
+
+def carregar_consultas() -> dict:
+    dados = carregar_json(caminhos.JSON_CONSULTAS, "consultas")
+    return dados
+
+def carregar_bloqueios() -> dict:
+    ...
+
+def salvar_pacientes(dados: dict) -> bool:
+    dados_salvar = salvar_json(caminhos.JSON_PACIENTES, "pacientes", dados)
+    return dados_salvar
+
+def salvar_medicos(dados: dict) -> bool:
+    dados_salvar = salvar_json(caminhos.JSON_MEDICOS, "medicos", dados)
+    return dados_salvar
+
+def salvar_consultas(dados: dict) -> bool:
+    dados_salvar = salvar_json(caminhos.JSON_CONSULTAS, "consultas", dados)
+    return dados_salvar
+
+
+
+
+
+
+
+
+
+
+
+
+
+def inicializar_banco() -> None:
+    ...
+
+def gerar_backup(caminho_arquivo):
+    ...
+
+def restaurar_backup():
+    ...
+
+
+
+    
