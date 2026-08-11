081e2621b37413d48412a0dba0890c6b89152d59
Guilherme Zacarias de Andrade <guilhermeza360@gmail.com>
2026-08-11 15:43:01 -0300

    Update geradorHash.py

--- chd/functions/hash/geradorHash.py
+++ chd/functions/hash/geradorHash.py
@@ -3,7 +3,7 @@ from hashlib import sha256
 def geradorHash(senha):
     """Função que gera um hash com base em uma string
     encode('utf-8') - transforma uma string em bytes
-    sha256 cria um objeto hash
+    sha256() cria um objeto hash
     hexdigest() transforma o objeto em uma string hexadecimal"""
 
     hash = sha256(senha.encode('utf-8')).hexdigest()
